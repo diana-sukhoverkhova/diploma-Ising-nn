894361e55bed3b44acdb6d8aefa8d3800a5c933b
diana-sukhoverkhova <diana.suhoverhova@mail.ru>
2022-06-27 01:44:42 +0300

    Changes in data generation files

--- ds.py
+++ ds.py
@@ -7,6 +7,7 @@ from data_simulate import simulate
 
 
 L = int(sys.argv[1])
+
 roots = [2.2691853142129728, 2.104982167992544, 1.932307699120554, 1.749339162933206, 1.5536238493280832, 1.34187327905057, 1.109960313758399, 0.8541630993606272, 0.5762735442012712, 0.2885386111960936, 0.03198372863548067]
 jds = [0.0, -0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1.0]
 get_crit_T = dict(zip(jds, roots))
@@ -14,9 +15,9 @@ get_crit_T = dict(zip(jds, roots))
 Jd = float(sys.argv[2])
 T_c = get_crit_T[Jd]
 num_temps = int(sys.argv[4])
-#T = np.linspace(T_c - 0.3, T_c + 0.3, num_temps)
-#T = np.round(T, 4)
-T = np.round(np.linspace(0.03, 3.5, num_temps), 4)
+T = np.linspace(T_c - 0.3, T_c + 0.3, num_temps)
+T = np.round(T, 4)
+#T = np.round(np.linspace(0.03, 3.5, num_temps), 4)
 idx = int(sys.argv[3])
 T = T[idx]
 
@@ -59,6 +60,6 @@ def smlt():
         np.save(f'data_spins/{Jd}_test/spins_{L}_{T}.npy', spins)
         np.save(f'data_spins/{Jd}_test/answ_{L}_{T}.npy', answ)
         end = time.time()
-        print('Training data was generated in', end - start, flush=True)
+        print('Testing data was generated in', end - start, flush=True)
 
 smlt()
--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=t50-60-00
-#SBATCH --error=slurms/t50-60-00-%a.err
-#SBATCH --output=slurms/t50-60-00-%a.out
-#SBATCH --array=0-49
+#SBATCH --job-name=t100-20-01
+#SBATCH --error=slurms/t100-20-01-%a.err
+#SBATCH --output=slurms/t100-20-01-%a.out
+#SBATCH --array=0-99
 
-L=60
+L=20
 echo $L
-Jd=0.0
+Jd=-0.1
 echo $Jd
-num_temps=50
+num_temps=100
 echo $num_temps
 
 idx=$SLURM_ARRAY_TASK_ID
